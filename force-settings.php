3d99f0efdb628a41ff08558bb7c8410616650c9c
Steve Ryan <steve.ryan@asu.edu>
2018-12-13 11:13:28 -0700

    So, we might want to go the other way with this… :-)

--- force-settings.php
+++ force-settings.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:     ASU Engineering - Force Classic Editor Coexistence
  * Plugin URI:      https://github.com/asuengineering/force-classic-editor-settings
- * Description:     Plugin to force the settings of the Classic Editor plugin to always display the Gutenberg editor. Coexist.
+ * Description:     Plugin to force the settings of the Classic Editor plugin to never display the Gutenberg editor.
  * Author:          Steve Ryan, ASU Engineering
  * Author URI:      https://engineering.asu.edu
  * Text Domain:     asu-engineering-force-classic-editor-coexistence
@@ -17,11 +17,11 @@
 add_action( 'plugins_loaded', 'asufse_force_editor_settings', 1, 0 );
 function asufse_force_editor_settings() {
 	if ( function_exists( 'classic_editor_init_actions' ) ) {
-		// Enforces the default option of "no-replace."
+		// Enforces the default option of "replace."
 		add_filter( 'pre_option_classic-editor-replace', 'asufse_force_coexistence' );
 	}
 }
 
 function asufse_force_coexistence( $value ) {
-	return 'no-replace';
+	return 'replace';
 }
